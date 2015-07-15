b80663966d78d6c84453a9278801a66f3a4e7825
jforbes <jforbes@brightcove.com>
2015-07-15 15:40:06 -0400

    Enable MSE in FF for videojs-contrib-dash

--- src/protractor.config.js
+++ src/protractor.config.js
@@ -10,88 +10,65 @@ if (process.env.CI !== 'false') {
 
   config.maxSessions = 6;
 
-  config.multiCapabilities = [{
-    browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
-  }, {
-    browserName: 'chrome',
-    platform: 'Windows 8.1',
-    chromeOptions: {
-      // http://peter.sh/experiments/chromium-command-line-switches/
-      args: [
-        // Disables installation of default apps on first run. This is used during automated testing.
-        'disable-default-apps',
-        // Disables default browser checking
-        'no-default-browser-check',
-        // Skip First Run tasks
-        'no-first-run',
-        // Disables the sandbox for all process types that are normally sandboxed.
-        'no-sandbox',
-        // Disables extensions
-        'disable-extensions'
-      ]
-    }
-    // }, {
-    //   browserName: 'firefox',
-    //   platform: 'Linux',
-    //   loggingPrefs: {
-    //     browser: 'SEVERE'
-    //   }
-  }].map(function(browser) {
-    if (process.env.TRAVIS) {
-      browser.name = process.env.TRAVIS_BUILD_NUMBER + process.env.TRAVIS_BRANCH;
-    } else {
-      browser.name = browser.browserName + '-' + (browser.version || 'latest') + '-' + browser.platform;
-    }
-
-    browser.build =process.env.BUILD ;
-    browser['tunnel-identifier'] = process.env.TUNNEL_ID ;
-    browser.recordVideo = false;
-    browser.recordScreenshots = false;
-    return browser;
-  });
-
   // Max time(sec) for a test suite to run on a VM
   config.maxDuration = 180;
-} else {
-  config.getMultiCapabilities = function() {
-    var deferred = q.defer();
-    firefoxProfile.setPreference('plugin.state.silverlight', 2);
-    firefoxProfile.updatePreferences();
-    firefoxProfile.encoded(function(encodedProfile) {
-      var multiCapabilities = [{
+}
+
+config.getMultiCapabilities = function() {
+  var deferred = q.defer();
+  firefoxProfile.setPreference('plugin.state.silverlight', 2);
+  firefoxProfile.setPreference('media.mediasource.whitelist', false);
+  firefoxProfile.updatePreferences();
+  firefoxProfile.encoded(function(encodedProfile) {
+    var multiCapabilities = [];
+
+    if (process.env.CI !== 'false') {
+      multiCapabilities = [{
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
+        version: '11'
+      }, {
         browserName: 'chrome',
-        chromeOptions: {
-          // http://peter.sh/experiments/chromium-command-line-switches/
-          args: [
-            // Disables installation of default apps on first run. This is used during automated testing.
-            'disable-default-apps',
-            // Disables default browser checking
-            'no-default-browser-check',
-            // Skip First Run tasks
-            'no-first-run',
-            // Disables the sandbox for all process types that are normally sandboxed.
-            'no-sandbox',
-            // Disables extensions
-            'disable-extensions'
-          ]
-        }
+        platform: 'Windows 8.1'
+          // }, {
+          //   browserName: 'firefox',
+          //   platform: 'Linux',
+          //   firefox_profile: encodedProfile,
+          //   loggingPrefs: {
+          //     browser: 'SEVERE'
+          //   }
+      }];
+    } else {
+      multiCapabilities = [{
+        browserName: 'chrome',
       }, {
         browserName: 'firefox',
-        // Always allow Silverlight
         firefox_profile: encodedProfile,
         loggingPrefs: {
           browser: 'SEVERE'
         }
       }];
+    }
 
-      deferred.resolve(multiCapabilities);
+    multiCapabilities = multiCapabilities.map(function(browser) {
+      if (process.env.TRAVIS) {
+        browser.name = process.env.TRAVIS_BUILD_NUMBER + process.env.TRAVIS_BRANCH;
+      } else {
+        browser.name = browser.browserName + '-' + (browser.version || 'latest') + '-' + browser.platform;
+      }
+
+      browser.build = process.env.BUILD;
+      browser['tunnel-identifier'] = process.env.TUNNEL_ID;
+      browser.recordVideo = false;
+      browser.recordScreenshots = false;
+      return browser;
     });
 
-    return deferred.promise;
-  };
-}
+    deferred.resolve(multiCapabilities);
+  });
+
+  return deferred.promise;
+};
 
 config.baseUrl = 'http://' + ip + ':7777';
 
