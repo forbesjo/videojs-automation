2a77c802d6698398cfacd00fb3e0ee6511bd75e9
jforbes <jforbes@brightcove.com>
2015-07-23 09:24:08 -0400

    skip dash tests

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ var url = require('url'),
   Player = require('../src/player');
 
 [
-  'videojs',
-  'videojs-contrib-dash'
+  'videojs'
+  // , 'videojs-contrib-dash'
 ].map(function(p) {
   return describe(p + ' Player', function() {
     var playerUrl = url.resolve(browser.baseUrl, 'test/' + p + '.html');
