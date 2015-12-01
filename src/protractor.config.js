b9a5a47da33724af5ebc2b64fb178fda24e4b01c
jforbes <jforbes@brightcove.com>
2015-12-01 17:55:32 -0500

    Remove iedriver version

--- src/protractor.config.js
+++ src/protractor.config.js
@@ -25,28 +25,26 @@ exports.config = {
           os: 'Windows',
           os_version: '8.1',
           browser_version: '11',
-          'browserstack.ie.driver': '2.48'
         }, {
           browserName: 'internet explorer',
           os: 'Windows',
           os_version: '7',
           browser_version: '11',
-          'browserstack.ie.driver': '2.48'
-            // }, {
-            //   browserName: 'internet explorer',
-            //   os: 'Windows',
-            //   os_version: '7',
-            //   browser_version: '10',
-            // }, {
-            //   browserName: 'internet explorer',
-            //   os: 'Windows',
-            //   os_version: '7',
-            //   browser_version: '9',
-            // }, {
-            //   browserName: 'internet explorer',
-            //   os: 'Windows',
-            //   os_version: '7',
-            //   browser_version: '8',
+          // }, {
+          //   browserName: 'internet explorer',
+          //   os: 'Windows',
+          //   os_version: '7',
+          //   browser_version: '10',
+          // }, {
+          //   browserName: 'internet explorer',
+          //   os: 'Windows',
+          //   os_version: '7',
+          //   browser_version: '9',
+          // }, {
+          //   browserName: 'internet explorer',
+          //   os: 'Windows',
+          //   os_version: '7',
+          //   browser_version: '8',
         }, {
           browserName: 'safari',
           os: 'OS X',
